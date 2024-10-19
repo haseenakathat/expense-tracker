cfb1e6d4b2c63feb1fc2df669c8d655eab2e2d76
Haseena Kathat <haseenakathat96@gmail.com>
2024-10-19 19:13:14 +0530

    Add files via upload

---
+++ Expense.js
@@ -0,0 +1,29 @@
+// const mongoose = require('mongoose');
+
+// const expenseSchema = new mongoose.Schema({
+//     category: { type: String, required: true },
+//     amount: { type: Number, required: true },
+//     date: { type: String, required: true },
+//     description: String
+// });
+
+// const Expense = mongoose.model('Expense', expenseSchema);
+// module.exports = Expense;
+
+
+
+
+
+
+// models/Expense.js
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+    category: { type: String, required: true },
+    amount: { type: Number, required: true },
+    date: { type: String },
+    description: { type: String, required: true }
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+module.exports = Expense;
